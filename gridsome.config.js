9859b5380a19d3181f8cd0bc98315f47b1eefb96
maykondeykon <maykondeykon@gmail.com>
2023-06-29 16:42:46 +0100

    siteURL config name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'MkDk Studio',
+  siteUrl: 'https://mkdkstudio.github.io',
   plugins: []
 }
