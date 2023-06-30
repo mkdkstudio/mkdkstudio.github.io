b0e671509799c9d331d24dbd7a2b52bda62c9e94
maykondeykon <maykondeykon@gmail.com>
2023-06-30 16:54:29 +0100

    Adding first images

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,13 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>MkDk Studio</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <g-image alt="Example image" src="~/assets/logo-1.png" />
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'MkDk Studio!'
   }
 }
 </script>
