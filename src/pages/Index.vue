6340df1e92f745b55a23ca32e7eb17e8ae97a1ae
maykondeykon <maykondeykon@gmail.com>
2023-07-01 16:59:18 +0100

    Update usage of component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,28 +1,21 @@
 <template>
   <div>
     <div class="page-header clear-filter" filter-color="orange">
-      <parallax class="page-header-image" style="background-image:url('img/header.jpg')">
-      </parallax>
+      <BaseParallax class="page-header-image" style="background-image:url('../assets/img/header.jpg')"/>
       <div class="container">
         <div class="content-center brand">
           <img class="n-logo" src="../assets/img/logo-1.png" alt="" />
           <h1 class="h1-seo">Now UI Kit.</h1>
           <h3>A beautiful Bootstrap 4 UI kit. Yours free.</h3>
         </div>
-        <h6 class="category category-absolute">
-          Designed by
-          <a href="http://invisionapp.com/" target="_blank">
-            <img src="img/invision-white-slim.png" class="invision-logo" /> </a>. Coded by
-          <a href="https://www.creative-tim.com" target="_blank">
-            <img src="img/creative-tim-white-slim2.png" class="creative-tim-logo"ß /> </a>.
-        </h6>
+        
       </div>
     </div>
-    <div class="main">
+    <div class="main">ß
       <div class="section section-images">
         <div class="container">
           <div class="row">
-            <div class="col-md-12">
+            <!-- <div class="col-md-12">
               <div class="hero-images-container">
                 <img src="img/hero-image-1.png" alt="" />
               </div>
@@ -32,7 +25,7 @@
               <div class="hero-images-container-2">
                 <img src="img/hero-image-3.png" alt="" />
               </div>
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
@@ -67,25 +60,10 @@
   </div>
 </template>
 <script>
-import Parallax from '~/components/Parallax.vue';
-// import BasicElements from './components/BasicElementsSection';
-// import Navigation from './components/Navigation';
-// import TabsSection from './components/Tabs';
-// import ProgressPagination from './components/ProgressPagination';
-// import Notifications from './components/Notifications';
-// import Typography from './components/Typography';
-// import JavascriptComponents from './components/JavascriptComponents';
-// import CarouselSection from './components/CarouselSection';
-// import NucleoIconsSection from './components/NucleoIconsSection';
-// import ExamplesSection from './components/ExamplesSection';
-// import DownloadSection from './components/DownloadSection';
 
 export default {
   name: 'index',
-  bodyClass: 'index-page',
-  components: {
-    Parallax
-  }
+  bodyClass: 'index-page'
 };
 </script>
 <style></style>
