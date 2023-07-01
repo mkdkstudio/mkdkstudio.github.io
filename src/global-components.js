982fe50b3c9648e58ad2b0c5242f9ab705346d23
maykondeykon <maykondeykon@gmail.com>
2023-07-01 18:55:53 +0100

    All Cleaned

--- src/global-components.js
+++ src/global-components.js
@@ -1,3 +1,17 @@
+import Vue from 'vue';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Use a função require.context para importar os componentes da pasta 'components'.
+const requireComponent = require.context(
+  './components', // Caminho da pasta onde estão os componentes
+  false, // Se deve olhar nas subpastas também
+  /Base[A-Z]\w+\.(vue|js)$/ // Expressão regular para selecionar os componentes pelo nome. Neste caso, estamos importando componentes que começam com 'Base' e terminam com .vue ou .js.
+);
+
+// Para cada componente importado, registre-o globalmente.
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+  const componentName = fileName.split('/').pop().replace(/\.\w+$/, '');
 
   Vue.component(componentName, componentConfig.default || componentConfig);
 });
--- src/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    css: {
-      loaderOptions: {
-        sass: {
-          additionalData: `@import "@/assets/scss/now-ui-kit.scss";`,
-        },
-      },
-    },
-  };
