2d1c80ee9356411122fb3f17eba3b993ccad9adc
maykondeykon <maykondeykon@gmail.com>
2023-07-01 19:03:35 +0100

    Adding logo

--- src/components/BaseFullPageBackground.vue
+++ src/components/BaseFullPageBackground.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="full-page-background">
+      <!-- Adicione a imagem de logo centralizada -->
+      <img class="logo" src="../assets/img/logo-1.png" alt="Logo">
       <!-- Conteúdo do componente aqui, se necessário -->
     </div>
   </template>
@@ -13,5 +15,22 @@
     
     /* Define a altura do componente para ocupar toda a tela */
     height: 100vh;
+    
+    /* Centraliza o conteúdo verticalmente */
+    display: flex;
+    align-items: center;
+  }
+  
+  /* Estilos da imagem de logo */
+  .logo {
+    /* Defina o tamanho da imagem de logo conforme necessário */
+    width: 500px;
+    height: auto;
+    
+    /* Centraliza a imagem horizontalmente */
+    margin: 0 auto;
+
+    /* Aplica o filtro para inverter as cores (preto vira branco, branco vira preto) */
+  filter: invert(1);
   }
   </style>
